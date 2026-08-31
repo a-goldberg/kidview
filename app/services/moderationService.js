794aad65a027781d0362801283c83dac47df0fc1
Adam Goldberg <a-goldberg@users.noreply.github.com>
2026-08-30 22:30:16 -0500

    Improving the extreme risk pattern matching to exclude inappropriate content with regex alone

--- app/services/moderationService.js
+++ app/services/moderationService.js
@@ -25,7 +25,7 @@ const RULE_MODEL_NAME = "rule-based-v1"
 const RULE_PROMPT_VERSION = "rules-v1"
 
 const SEVERE_RISK_PATTERN =
-  /suicide|self[- ]?harm|porn|sexual|gore|graphic|murder|kill|weapon|gun|knife|flamethrower|poison|toxin|skyscraper|rooftop/i
+  /suicide|self[- ]?harm|p[0o]{1}rn|pr[o0]{1}n|pstars|prnstars|x{3,}|bbw|boot(y|ie)|butt(?!er)|\banal\b|brazzers|\btit(s|ties)?\b|\bsexx?(y|ual)?|nak[ei]d|boobies|\bdicks?\b|\brape(\b|s)|\bslut|gore|murder|kill|weapon|gun|knife|flamethrower|poison|toxin|skyscraper|rooftop/i
 const RISK_PATTERN =
   /scary|secret|secrets|exposed|drama|breakup|rumor|prank|challenge|mystery box|unboxing|haul|shopping|spent \$|won't believe|do not try|dangerous|gaming|minecraft|roblox|fortnite|dark fantasy|pvp|boob|breasts|graphic/i
 const CLICKBAIT_PATTERN =
